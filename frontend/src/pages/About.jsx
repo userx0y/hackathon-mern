f244d76bdf51486b088582903131eec954bf71ba
userx0y <userx0y@gmail.com>
2025-08-19 23:53:40 -0500

    Changes in aboutpage

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,14 +10,12 @@ const About = () => {
     <div className="container-fluid pt-5">
         <h1 className="ps-5" data-aos="flip-left" style={{fontFamily: "'Montserrat'", fontSize: '70px'}}>About us</h1>
         <div className="d-flex justify-content-start p-5">
-        <p style={{ textAlign: "justify", fontSize: '20px'}}data-aos="flip-down">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus hic, reiciendis perspiciatis voluptatum, mollitia maxime dolores recusandae commodi voluptatibus eos blanditiis consequuntur. In asperiores quam illo nam rem! Ducimus, eos.
-            Ut esse doloribus dolor cupiditate qui voluptates omnis quaerat, veniam natus corrupti dignissimos mollitia, consequatur molestiae. Cupiditate est reprehenderit eius tenetur maiores odio, animi suscipit harum eligendi, iste ullam nihil?
-            Placeat reiciendis nulla voluptates sint sapiente minus ut repudiandae. Quo illum rerum magnam ullam aspernatur at maiores. Saepe nobis esse ab officiis voluptatibus aspernatur ipsam recusandae blanditiis. Veniam, hic molestias!
-            Debitis magni deserunt ad, esse at illo mollitia provident ipsam tempore eaque repellendus sed doloribus aliquid assumenda possimus asperiores, ducimus voluptates nihil distinctio id expedita perferendis enim amet? A, ea?
-            Sapiente sint non explicabo obcaecati, excepturi illo? Repellat sit repellendus modi delectus ut consectetur nesciunt doloremque obcaecati. Sed dolorum sint nulla provident dicta, laudantium animi ipsam. Consequatur autem ducimus at!
-            Velit, sequi temporibus ea voluptatem cum et molestiae mollitia atque vitae optio pariatur sit, beatae natus, reiciendis hic suscipit aliquid ab deleniti voluptatum perspiciatis repudiandae vel! Similique, minus ut. Ullam?
-            Minus temporibus magni repellat nesciunt aliquam quae veritatis praesentium eius blanditiis nulla itaque ex vitae quibusdam expedita distinctio sit, sapiente quas harum quisquam doloribus eum atque! Inventore natus tempora aliquam.
+        <p style={{ textAlign: "justify", fontSize: '20px'}} data-aos="flip-down">
+            Welcome to our College Yearbook Page Builder! This project lets students create their own personalized yearbook pages with a simple drag-and-drop interface. You can add images, write quotes, and arrange them however you like to make your page truly unique.  
+
+            Our goal is to make yearbooks more interactive and fun, giving everyone a chance to express themselves creatively. Whether it’s sharing memories, adding favorite quotes, or decorating with photos, this platform makes it easy for students to design their page exactly the way they imagine.  
+
+            We hope this tool brings joy and nostalgia, capturing the moments that make college life memorable.
         </p>
         </div>
     </div>
